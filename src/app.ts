6e4004e93a209f8657b3ce4cdd57fb5dcfb5b493
pulok-thedeveloper <mdpulokhasan@gmail.com>
2025-09-15 18:56:17 +0600

    server setup db connected done

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to Ride Sharing App Backend",
+  });
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,65 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import { envVars } from "./app/config/env";
+import app from "./app";
+
+let server: Server;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(envVars.DB_URL);
+
+    console.log("Connected to DB!");
+
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is listening to port ${envVars.PORT}`);
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection Detected... Server shutting down...", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception Detected... Server shutting down...", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+
+process.on("SIGTERM", (err) => {
+  console.log("SIGTERM Signal Recieved... Server shutting down...", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("SIGINT", (err) => {
+  console.log("SIGINT Signal Recieved... Server shutting down...", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
